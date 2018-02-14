cba401d56ea94de17c9e54c676c8f75e40131984
Funsis <s.korovin95@gmail.com>
2018-02-14 19:14:03 +0300

    Create Game component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header/Header";
+import Game from "../Game/Game";
 import Footer from "../Footer/Footer";
 
 const App = () => {
   return (
     <div>
      <Header />
+     <Game />
      <Footer />
     </div>
   );
---
+++ src/components/Game/Game.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Game = () => (
+  <div className="game">
+    <div>Game Control</div>
+    <div>Board</div>
+    <div>Board Setings</div>
+  </div>
+);
+
+export default Game;
