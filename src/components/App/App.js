79bbe31e611c42d4e7239f07bb4aae16449df892
Funsis <s.korovin95@gmail.com>
2018-02-14 18:05:44 +0300

    Create Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const App = () => {
   return (
     <div>
      <Header />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer class="footer">
+    <div class="footer__content">
+      Feel free to add cells while it's running. The cells in light red are
+      younger, dark red are older. Enjoy!
+    </div>
+  </footer>
+);
+
+export default Footer;
