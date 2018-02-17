3cec3d28dd96930903310dacb49293a2e08c04e5
Funsis <s.korovin95@gmail.com>
2018-02-17 15:53:37 +0300

    Create Notice component

---
+++ src/components/Notice/Notice.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Notice = () => (
+  <div className="notice">
+    Board Size: Sim Speed: Feel free to add cells while it's running. The cells
+    in light red are younger, dark red are older. Enjoy!
+  </div>
+);
