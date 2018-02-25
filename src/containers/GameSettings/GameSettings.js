5f18b3ff6cc298e294db5fc1643b9b171ea2da4a
Funsis <s.korovin95@gmail.com>
2018-02-25 20:26:07 +0300

    Add some styles

--- src/containers/GameSettings/GameSettings.js
+++ src/containers/GameSettings/GameSettings.js
@@ -2,22 +2,23 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import Button from "../../components/Button/Button";
+import "./GameSettings.css";
 
 class GameSettings extends Component {
   render() {
     return (
       <div className="game-settings">
         <div className="game-settings__content">
-          <p className="game-setings__content-text">Board size:</p>
-          <div className="game-setings__content--buttons">
+          <p className="game-settings__content-text">Board size:</p>
+          <div className="game-settings__content-buttons">
             <Button name="50x30" />
             <Button name="70x50" />
             <Button name="100x80" />
           </div>
         </div>
         <div className="game-settings__content">
-          <p className="game-setings__content-text">Sim Speed:</p>]
-          <div className="game-setings__content--buttons">
+          <p className="game-settings__content-text">Sim Speed:</p>
+          <div className="game-settings__content-buttons">
             <Button name="Slow" />
             <Button name="Medium" />
             <Button name="Fast" />
